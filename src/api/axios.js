dff7adf23272a4f46b9b189e690b5d52a65394d1
irmasaquiza <nachitomv2005@hotmail.com>
2026-04-27 20:00:29 -0500

    conectada con la  api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://localhost:44361/api/v1',
+  //baseURL: 'https://localhost:44361/api/v1',
+  baseURL: 'https://microserviciovuelosapi20260421231751-ebfchba0auh0ekat.brazilsouth-01.azurewebsites.net/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
